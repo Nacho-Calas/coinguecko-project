5a591f5c4b39509be297beff420601c303cfe3e3
Nacho-Calas <nachocalas@hotmail.com>
2023-02-08 12:24:21 -0300

    bug fixing + loader

--- client/src/pages/CoinDetail/index.tsx
+++ client/src/pages/CoinDetail/index.tsx
@@ -7,8 +7,7 @@ import style from "./index.module.css";
 
 function CoinDetails() {
   const { id } = useParams();
-
-  const { coin, getCoinDetail } = useGetDetail();
+  const { coin, isLoading, getCoinDetail } = useGetDetail();
 
   useEffect(() => {
     if (id) {
@@ -16,47 +15,51 @@ function CoinDetails() {
     }
   }, [id]);
 
-  /* if (!coin) return <h1 className={style.loading}>Loading...</h1>; */
-
   return (
-    <div className={style.container}>
-      <div className={style.header}>
-        <div className={style.title}>
-          <h1>{`#${coin?.market_cap_rank}`}</h1>
-          <img src={coin?.image.small} alt={coin?.name} />
-          <h1>{coin?.name}</h1>
-          <h2>{coin?.symbol}</h2>
-        </div>
-        <div className={style.price}>
-          <h1>{`${coin?.market_data.current_price.usd} USD`}</h1>
-        </div>
+    <>
+      {isLoading ? (
+        <div className={style.loader}></div>
+      ) : (
+        <div className={style.container}>
+          <div className={style.header}>
+            <div className={style.title}>
+              <h1>{`#${coin?.market_cap_rank}`}</h1>
+              <img src={coin?.image.small} alt={coin?.name} />
+              <h1>{coin?.name}</h1>
+              <h2>{coin?.symbol}</h2>
+            </div>
+            <div className={style.price}>
+              <h1>{`${coin?.market_data.current_price.usd} USD`}</h1>
+            </div>
 
-        <div className={style.market}>
-          <h3 className={style.high}>
-            {" "}
-            {`High 24h: ${coin?.market_data.high_24h?.usd} USD`}
-          </h3>
-          <h3 className={style.low}>
-            {" "}
-            {`Low 24h: ${coin?.market_data.low_24h?.usd} USD`}
-          </h3>
-        </div>
+            <div className={style.market}>
+              <h3 className={style.high}>
+                {" "}
+                {`High 24h: ${coin?.market_data.high_24h?.usd} USD`}
+              </h3>
+              <h3 className={style.low}>
+                {" "}
+                {`Low 24h: ${coin?.market_data.low_24h?.usd} USD`}
+              </h3>
+            </div>
 
-        <div className={style.market}>
-          <h3>{`Market Cap: ${currencyFormat(
-            coin?.market_data.market_cap?.usd || 0
-          )} USD`}</h3>
-          <h3>{`Total Volume: ${currencyFormat(
-            coin?.market_data.total_volume?.usd || 0
-          )} USD`}</h3>
-        </div>
-      </div>
+            <div className={style.market}>
+              <h3>{`Market Cap: ${currencyFormat(
+                coin?.market_data.market_cap?.usd || 0
+              )} USD`}</h3>
+              <h3>{`Total Volume: ${currencyFormat(
+                coin?.market_data.total_volume?.usd || 0
+              )} USD`}</h3>
+            </div>
+          </div>
 
-      <div className={style.description}>
-        <h1>Description</h1>
-        <p>{coin?.description?.en}</p>
-      </div>
-    </div>
+          <div className={style.description}>
+            <h1>Description</h1>
+            <p>{coin?.description?.en}</p>
+          </div>
+        </div>
+      )}
+    </>
   );
 }
 
--- client/src/types/coinType.ts
+++ client/src/types/coinType.ts
@@ -3,7 +3,8 @@ export interface Coin {
     id: string;
     symbol: string;
     name: string;
-    image: string;
+    image: any;
+    market_data: any;
     current_price: number;
     market_cap: number;
     market_cap_rank: number;
@@ -13,6 +14,7 @@ export interface Coin {
     price_change_24h: number;
     price_change_percentage_24h: number;
     market_cap_change_24h: number;
+    description: any;
   }
 
   
